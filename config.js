48d81712d17c16cfa7b3d7ed2fc90b0c0465fda2
genecarles <genecarles@gmail.com>
2026-05-24 19:16:02 +0200

    ssss

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ const CONFIG = {
     URL_OFICIAL:    "https://agora.altervector.com",
 
     // 2.2 RUTES Imatges
-    BACKGROUND:      "",
+    BACKGROUND:      "images/background-agora.jpg",
     BLOC_HERO:       "images/hero-agora.png",
     BLOC1:           "images/diari.png",
     BLOC2:           "images/finde.png",
